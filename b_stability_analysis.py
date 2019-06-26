80c0148945881109b1519afca4ed8cf01d5355c1
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-06-26 18:14:55 +0900

    fix stability analysis

--- b_stability_analysis.py
+++ b_stability_analysis.py
@@ -24,16 +24,16 @@ def summarize_stat_fixed_number(number, statistic='type'):
     part1 = 'cor_pca2d_logmerg_m0_5wdw1shft_y1976_to_y2005_'
     part2 = 'n' + str(number) + '_'
     part3 = 'o(0\.[0-9]+)_'
-    part4 = 'firstgap_flare_stats\.txt'
+    part4 = 'firstgap_flare_stats\.csv'
 
     pattern = re.compile(part1+part2+part3+part4)
 
     firms = get_firms()
     data = pandas.DataFrame(index=firms)
-    for flare_file in folder.glob("*/*flare_stats.txt"):
-        print(flare_file.name)
+    for flare_file in folder.glob("*/*flare_stats.csv"):
         match = pattern.match(flare_file.name)
         if match:
+            print(flare_file.name)
             overlap = float(match.group(1))
             raw = pandas.read_csv(str(flare_file),index_col=0).loc[:,statistic]
             data[overlap] = raw
@@ -56,9 +56,9 @@ def summarize_stat_fixed_overlap(overlap, statistic='type'):
     firms = get_firms()
     data = pandas.DataFrame(index=firms)
     for flare_file in folder.glob("*/*flare_stats.csv"):
-        print(flare_file.name)
         match = pattern.match(flare_file.name)
         if match:
+            print(flare_file.name)
             n = int(match.group(1))
             raw = pandas.read_csv(str(flare_file),index_col=0).loc[:,statistic]
             data[n] = raw
@@ -75,6 +75,8 @@ if __name__ == "__main__":
     # summarize_stat_fixed_number(20, 'k_index')
     # summarize_stat_fixed_overlap(50, 'k_index')
 
-    # summarize_stat_fixed_number(20)
+    stab = summarize_stat_fixed_number(15)
+    stab.to_csv(str(folder.joinpath("stab_cor_pca2d_logmerg_m0_5wdw1shft_n15.csv")))
+
     stab = summarize_stat_fixed_overlap(50)
     stab.to_csv(str(folder.joinpath("stab_cor_pca2d_logmerg_m0_5wdw1shft_o.50.csv")))
