cd98519d6c4a0a989caf80e660f3d1513aa4a1c7
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-01-29 13:35:25 +0900

    factor out file naming for stability analysis

--- b_stability_analysis.py
+++ b_stability_analysis.py
@@ -4,10 +4,19 @@ import numpy as np
 
 import pandas
 
-folder = pathlib.Path("./cor_pca2d_logmerg/m0")
+if False:
+    data_folder = "180901_csv/"
+    data_code = "D0"
+else:
+    data_folder = "200110_csv/"
+    data_code = "D1"
+
+folder = pathlib.Path("./cor_pca2d_logmerg/{}m0".format(data_code))
+basename = "cor_pca2d_logmerg_{}m0_5wdw1shft".format(data_code)
+
 
 def get_firms():
-    data = pandas.read_csv("180901_csv/190204_firm_rank_name_industry.csv",dtype=str)
+    data = pandas.read_csv(data_folder + "firm_rank_name_industry.csv",dtype=str)
     firms = list(data.loc[:,'firm_name'])
     firms = [x.replace(" ", "_") for x in firms]
 
@@ -21,7 +30,7 @@ def remove_invalid_columns(data, firms):
 
 
 def summarize_stat_fixed_number(number, statistic='type'):
-    part1 = 'cor_pca2d_logmerg_m0_5wdw1shft_y1976_to_y2005_'
+    part1 = basename + "_y1976_to_y2005_"
     part2 = 'n' + str(number) + '_'
     part3 = 'o(0\.[0-9]+)_'
     part4 = 'firstgap_flare_stats\.csv'
@@ -46,7 +55,7 @@ def summarize_stat_fixed_number(number, statistic='type'):
     return data
 
 def summarize_stat_fixed_overlap(overlap, statistic='type'):
-    part1 = 'cor_pca2d_logmerg_m0_5wdw1shft_y1976_to_y2005_'
+    part1 = basename + "_y1976_to_y2005_"
     part2 = 'n([0-9]+)_'
     part3 = 'o0\.' + str(overlap) + '_'
     part4 = 'firstgap_flare_stats\.csv'
@@ -71,12 +80,32 @@ def summarize_stat_fixed_overlap(overlap, statistic='type'):
     return data
 
 
+def do_both_fixed_overlap(overlap):
+    stab = summarize_stat_fixed_overlap(overlap)
+    ofname = "stab_type_{}_o{:.2f}.csv".format(basename, overlap/100.)
+    stab.to_csv(str(folder.joinpath(ofname)))
+
+    stab = summarize_stat_fixed_overlap(overlap, statistic='k_index')
+    ofname = "stab_index_{}_o{:.2f}.csv".format(basename, overlap/100.)
+    stab.to_csv(str(folder.joinpath(ofname)))
+
+def do_both_fixed_number(number):
+    stab = summarize_stat_fixed_number(number)
+    ofname = "stab_type_{}_n{}.csv".format(basename, number)
+    stab.to_csv(str(folder.joinpath(ofname)))
+
+    stab = summarize_stat_fixed_number(number, statistic="k_index")
+    ofname = "stab_index_{}_n{}.csv".format(basename, number)
+    stab.to_csv(str(folder.joinpath(ofname)))
+
+
+
 if __name__ == "__main__":
     # summarize_stat_fixed_number(20, 'k_index')
     # summarize_stat_fixed_overlap(50, 'k_index')
 
-    stab = summarize_stat_fixed_number(15)
-    stab.to_csv(str(folder.joinpath("stab_cor_pca2d_logmerg_m0_5wdw1shft_n15.csv")))
+    # stab = summarize_stat_fixed_number(15)
+    # stab.to_csv(str(folder.joinpath("stab_cor_pca2d_logmerg_m0_5wdw1shft_n15.csv")))
 
-    stab = summarize_stat_fixed_overlap(50)
-    stab.to_csv(str(folder.joinpath("stab_cor_pca2d_logmerg_m0_5wdw1shft_o.50.csv")))
+    #do_both_fixed_overlap(50)
+    do_both_fixed_number(20)
