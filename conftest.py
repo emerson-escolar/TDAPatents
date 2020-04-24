0c07b99a9ee702c33ef1e47f0525b3514fbded41
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-04-24 16:48:30 +0900

    add source for test skipping

--- conftest.py
+++ conftest.py
@@ -1,15 +1,13 @@
 import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--comprehensive", action="store_true", default=False, help="run comprehensive tests"
-    )
+# https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option
 
+def pytest_addoption(parser):
+    parser.addoption("--comprehensive", action="store_true", default=False, help="run comprehensive tests")
 
 def pytest_configure(config):
     config.addinivalue_line("markers", ": mark test as part of comprehensive testing")
-
 
 def pytest_collection_modifyitems(config, items):
     if config.getoption("--comprehensive"):
