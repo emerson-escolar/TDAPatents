df47ba8e96f32af696dcf47040a269125396027d
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-06-03 14:30:01 +0900

    factor out datalabels file

--- a_patent_data.py
+++ a_patent_data.py
@@ -1,9 +1,10 @@
 import pathlib
 import numpy as np
 import pandas
-import json
 
 import collections
+
+from a_data_labels import DataLabels
 
 # This file contains functions to facilitate extraction of data from data package 180901_csv.
 # Format of data is assumed to be as given in those files.
@@ -20,82 +21,6 @@ class DataLocator(object):
     def get_fname_subbed(self, params):
         fname = self.fname_format.format(*params)
         return str(self.folder_path.joinpath(fname))
-
-
-class DataLabels(object):
-    def __init__(self):
-        self.extra_desc = ""
-        self.data_name = ""
-        self.transforms_name = ""
-        self.mode_name = ""
-
-        self.p_sizes = None # np.array or None
-        self.rgb_colors = None  # np.array or list or None
-        self.unique_members = None  # list or None
-
-        self.years_data = None   # numpy.array or None
-        self.intemporal_index = None  # pandas.Index or None
-
-    def to_json_fname(self, fname):
-        with open(fname, "w") as outfile:
-            self.to_json(outfile)
-
-    def to_json(self, fp):
-        json.dump(self, fp, cls=DataLabelsEncoder)
-
-
-    @classmethod
-    def from_json_fname(cls, fname):
-        with open(fname, "r") as infile:
-            return json.load(infile, object_hook=decode_data_labels)
-
-
-
-def decode_data_labels(dct):
-    if "__data_labels__" in dct:
-        ans = DataLabels()
-        ans.extra_desc = dct["extra_desc"]
-        ans.data_name = dct["data_name"]
-        ans.transforms_name = dct["transforms_name"]
-        ans.mode_name = dct["mode_name"]
-
-        def to_array(x):
-            return None if x is None else np.array(x)
-
-        ans.p_sizes = to_array(dct["p_sizes"])
-        ans.rgb_colors = to_array(dct["rgb_colors"])
-        ans.years_data = to_array(dct["years_data"])
-
-        ans.unique_members = dct["unique_members"]
-        ans.intemporal_index = None if dct["intemporal_index"] is None else pandas.Index(dct["intemporal_index"])
-        return ans
-
-    return dct
-
-
-class DataLabelsEncoder(json.JSONEncoder):
-    def default(self, labels):
-        if isinstance(labels, DataLabels):
-            data = {"__data_labels__": True,
-                    "extra_desc": labels.extra_desc,
-                    "data_name" : labels.data_name,
-                    "transforms_name": labels.transforms_name,
-                    "mode_name": labels.mode_name}
-            data["p_sizes"] = self._member_to_maybe_list(labels.p_sizes)
-            data["rgb_colors"] = self._member_to_maybe_list(labels.rgb_colors)
-            data["unique_members"] = self._member_to_maybe_list(labels.unique_members)
-            data["years_data"] = self._member_to_maybe_list(labels.years_data)
-            data["intemporal_index"] = self._member_to_maybe_list(labels.intemporal_index)
-            return data
-
-    @staticmethod
-    def _member_to_maybe_list(member):
-        if member is None or isinstance(member, list):
-            return member
-        elif isinstance(member, np.ndarray) or isinstance(member, pandas.Index):
-            return member.tolist()
-        return None
-
 
 
 def sum_columns(raw_data):
